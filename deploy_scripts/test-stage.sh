0cb29318b2f9c755f8833471dd86841b9a4da248
Dmitriy Krayniy <krdima92@gmail.com>
2018-03-05 18:28:39 +0300

    enable e2e tests

--- deploy_scripts/test-stage.sh
+++ deploy_scripts/test-stage.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-echo "e2e test in developing"
-#docker exec iqs-e2e powershell /iqs-management-ps-ws/iqs-simulation-ps/src/Test/Test-IqsQuotes.ps1 -ServerUrl "api.positron.stage.iquipsys.net"
+docker exec iqs-e2e powershell /iqs-management-ps-ws/iqs-simulation-ps/src/Test/Test-IqsQuotes.ps1 -ServerUrl "api.positron.stage.iquipsys.net"
