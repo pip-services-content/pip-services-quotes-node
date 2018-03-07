63f5c9fc2d2f5f6156914f8ed8b182fb55ef40df
stee1 <krdima92@gmail.com>
2018-03-07 10:04:13 +0000

    Travis build 45 updated build number in stage deploy file [skip ci]

--- deploy_scripts/upgrade-stage.sh
+++ deploy_scripts/upgrade-stage.sh
@@ -2,6 +2,6 @@
 
 COMPONENT=$(grep -m1 name package.json | tr -d '\r' | awk -F: '{ print $2 }' | sed 's/[", ]//g')
 VERSION=$(grep -m1 version package.json | tr -d '\r' | awk -F: '{ print $2 }' | sed 's/[", ]//g')
-BUILD_NUMBER=44-rc
+BUILD_NUMBER=45-rc
 
 ansible-playbook deploy_scripts/upgrade-stage.yml -u ubuntu -e COMPONENT=$COMPONENT -e VERSION=$VERSION -e BUILD_NUMBER=$BUILD_NUMBER --private-key=~/.ssh/iqsadmin.pem -vvvv
