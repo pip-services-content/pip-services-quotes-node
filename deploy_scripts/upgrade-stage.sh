284d65e6171c9c046e159798bf192eddacbc21a0
Dmitriy Krayniy <krdima92@gmail.com>
2018-03-13 12:43:22 +0300

    removed deploy scripts

--- deploy_scripts/upgrade-stage.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-COMPONENT=$(grep -m1 name package.json | tr -d '\r' | awk -F: '{ print $2 }' | sed 's/[", ]//g')
-VERSION=$(grep -m1 version package.json | tr -d '\r' | awk -F: '{ print $2 }' | sed 's/[", ]//g')
-BUILD_NUMBER=51-rc
-
-ansible-playbook deploy_scripts/upgrade-stage.yml -u ubuntu -e COMPONENT=${COMPONENT} -e VERSION=${VERSION} -e BUILD_NUMBER=${BUILD_NUMBER} --private-key=~/.ssh/iqsadmin.pem -vvvv
