2f648687be320d8f2c97df4677c8bf4f014fa217
pipdeveloper <pipdevs@gmail.com>
2018-05-24 08:03:06 +0000

    project build by Travis CI [skip ci]

--- obj/src/services/version1/QuotesHttpServiceV1.js
+++ obj/src/services/version1/QuotesHttpServiceV1.js
@@ -4,7 +4,7 @@ const pip_services_commons_node_1 = require("pip-services-commons-node");
 const pip_services_net_node_1 = require("pip-services-net-node");
 class QuotesHttpServiceV1 extends pip_services_net_node_1.CommandableHttpService {
     constructor() {
-        super('quotes');
+        super('v1/quotes');
         this._dependencyResolver.put('controller', new pip_services_commons_node_1.Descriptor('pip-services-quotes', 'controller', 'default', '*', '1.0'));
     }
 }
